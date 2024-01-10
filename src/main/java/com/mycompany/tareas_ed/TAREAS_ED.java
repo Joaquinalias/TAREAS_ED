334852662251b5dcf1f74fc4a4f9594d477db84b
Windows10 <Windows10@DESKTOP-F7L0247>
2024-01-10 13:54:26 +0100

    Esta es una prueba de commit

---
+++ src/main/java/com/mycompany/tareas_ed/TAREAS_ED.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tareas_ed;
+
+/**
+ *
+ * @author Windows10
+ */
+public class TAREAS_ED {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
